69871d687e2d69a884962631db298b081cedd077
Cenker <cenkerkumlucali0@gmail.com>
2021-05-26 18:23:25 +0300

    LogAspect added

---
+++ Core/Aspects/Autofac/Logging/LogAspect.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using Castle.DynamicProxy;
+using Core.CrossCuttingConcerns.Logging;
+using Core.CrossCuttingConcerns.Logging.Log4Net;
+using Core.Utilities.Interceptors;
+
+namespace Core.Aspects.Autofac.Logging
+{
+    public class LogAspect:MethodInterception
+    {
+        private LoggerServiceBase _loggerServiceBase;
+
+        public LogAspect(Type loggerService)
+        {
+            if (loggerService.BaseType!=typeof(LoggerServiceBase))
+            {
+                throw new Exception("Bu bir logger sınıfı değildir");
+            }
+            _loggerServiceBase = (LoggerServiceBase)Activator.CreateInstance(loggerService);
+        }
+
+        protected override void OnBefore(IInvocation invocation)
+        {
+            _loggerServiceBase.Info(GetLogDetail(invocation));
+        }
+
+        private LogDetail GetLogDetail(IInvocation invocation)
+        {
+            var logParameters = invocation.Arguments.Select(x => new LogParamater
+            {
+                Value = x,
+                Type = x.GetType().Name,
+
+            }).ToList();
+            var logDetail = new LogDetail
+            {
+                MethodName = invocation.Method.Name,
+                LogParamaters = logParameters,
+            };
+            return logDetail;
+        }
+    }
+}
