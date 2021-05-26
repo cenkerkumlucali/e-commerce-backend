7bffe35fecdff284bc9ba6a7d5dc31cd9b25b498
Cenker <cenkerkumlucali0@gmail.com>
2021-05-26 18:23:56 +0300

    FileLogger added

---
+++ Core/CrossCuttingConcerns/Logging/Log4Net/Loggers/FileLogger.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.CrossCuttingConcerns.Logging.Log4Net.Loggers
+{
+    public class FileLogger : LoggerServiceBase
+    {
+        public FileLogger() : base("JsonFileLogger")
+        {
+
+        }
+    }
+}
